5d77722797861caab39f8de9e72d69b281147225
igor <46280694+trueoss23@users.noreply.github.com>
2023-05-12 22:53:30 +0700

    Update main.py
    add class Books, add validations dates, add code errors and processing HTTP requests

--- main.py
+++ main.py
@@ -1,70 +1,69 @@
 from fastapi import FastAPI
 from fastapi import HTTPException
+from pydantic import BaseModel
 
 app = FastAPI(
     title="Library"
 )
 
+
+class Book(BaseModel):
+    book_id: int
+    name: str
+    year_published: int
+
+
 books = [
-    {"name": "War and Peace", "year_published": 1873, },
-    {"name": "Sapiens", "year_published": 2011, },
-    {"name": "Pride and prejudice", "year_published": 1813, },
+    Book(book_id=1, name="War and Peace", year_published=1873),
+    Book(book_id=2, name="Sapiens", year_published=2011),
+    Book(book_id=3, name="Pride and prejudice", year_published=1813),
 ]
 
 
-@app.get("/library/get_library")
+@app.get("/library")
 async def read_library():
-    return books
+    if books:
+        return books
+    raise HTTPException(status_code=500, detail="no database")
 
 
-@app.get("/libary/{book_name}")
-async def read_book(book_name: str):
-    res = []
+@app.get("/library/{book_id}", status_code=200)
+async def read_book(book_id: int):
     if books:
         for book in books:
-            if book["name"] == book_name:
-                res.append(book)
-    return res
+            if book.book_id == book_id:
+                return book
+    raise HTTPException(status_code=404, detail="no book with this id")
 
 
-@app.post("/libary/create")
-async def create_book(book: dict):
+@app.post("/library", status_code=201)
+async def create_book(book: Book):
     if books:
-        if book not in books:
-            if list(book.keys()) == list(books[0].keys()):
-                books.append(book)
-                return f"Added {book}"
-            else:
-                raise HTTPException(status_code=400, detail="incorrect book")
-        else:
-            return "book is already in library"
-    else:
         books.append(book)
-        # this weakness if we clean library and will add bad book
-        return f"Added {book}"
+    raise HTTPException(status_code=500, detail="no database")
 
 
-@app.put("/libary/update")
-async def update_book(book: dict, new_book: dict):
+@app.put("/library", status_code=200)
+async def update_book(book: Book, new_book: Book):
     if books:
         if book in books:
             i = books.index(book)
+            if books[i] == new_book:
+                raise HTTPException(status_code=304,
+                                    detail="no data to change")
             books[i] = new_book
-            return "OK"
-    return "Book is not found"
+    raise HTTPException(status_code=404, detail="no such book")
 
 
-@app.delete("/libary/delete")
-async def delete_book(book: dict):
+@app.delete("/library", status_code=204)
+async def delete_book(book: Book):
     if books:
         if book in books:
             books.remove(book)
-            return f"delete {book}"
-        return f"Not found {book}"
-    else:
-        return "library is empty"
+        raise HTTPException(status_code=404, detail="no such book for remove")
+    raise HTTPException(status_code=500, detail="no database")
 
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="0.0.0.0", port=8000, reload=True)
+    uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
